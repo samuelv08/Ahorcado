9f71f4ca3d49a4dd3d3ff278e5a912fa52c9505c
SamuelV08 <ssvillamizar2018@gmail.com>
2025-12-11 05:31:49 -0500

    complete todo el cuerpo y la estructura del ahorcado

--- my-react-app/src/App.jsx
+++ my-react-app/src/App.jsx
@@ -3,8 +3,16 @@ import './App.css'
 function App() {
   return (
     <>
-      <div className="circule"></div>
+      <div className="gallows-vertical"></div>
+      <div className="gallows-horizontal"></div>
+      <div className="gallows-rope"></div>
+      <div className="gallows-base"></div>
+      <div className="head"></div>
       <div className="chest"></div>
+      <div className="left-leg"></div>
+      <div className="right-leg"></div>
+      <div className="left-arm"></div>
+      <div className="right-arm"></div>
     </>
   )
 }
